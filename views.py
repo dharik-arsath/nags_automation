a8109d7781fe3b7e1bacab936f3ca68241aebbcb
Dharik Arsath <dharikarsath12@gmail.com>
2025-02-16 21:21:35 +0530

    telegram message update

--- views.py
+++ views.py
@@ -7,19 +7,17 @@ import json
 from notify_telegram import send_message
 import asyncio
 from loguru import logger
-from utils import generate_id
 from pathlib import Path
 
 from tabulate import tabulate
 from sheets.sheet import ProductSheetHandler
 from sheets.sheet_dto import ValidateSheetInfo, ValidateExpenseInfo, SheetManager
 from sheets.expense_sheet import ExpenseSheetHandler
-from sheets.sheet_helper import authenticate, get_numeric_id_from_main_sheet, get_sheet_by_id, get_workbook_by_id,generate_numeric_id
+from sheets.sheet_helper import authenticate, get_sheet_by_id, get_workbook_by_id,generate_numeric_id
 import concurrent.futures
 from functools import lru_cache
 import os 
 from flask import send_file
-from flask import Request
 
 gc = authenticate()
 workbook = get_workbook_by_id(gc, os.getenv("NAGS_WORKBOOK_ID"))
@@ -29,8 +27,103 @@ expense_sheet  = get_sheet_by_id(workbook, os.getenv("EXPENSE_SHEET_ID"))
 items_sheet_manager   = SheetManager(gc=gc, spreadsheet=workbook, worksheet=items_sheet)
 expense_sheet_manager = SheetManager(gc=gc, spreadsheet=workbook, worksheet=expense_sheet)
 
+# def format_msg(data):
+#     # Initialize variables
+#     total_amount = 0
+#     discount = 0
+#     commission = 0
+#     final_amount = 0
+#     expenses = {}
+#     entries = data["entries"]
+#     expenses = data["expenses"]
+#     credits = data.get("credits", {"payable": 0, "receivable": 0})
+    
+#     # Get driver name, line, and date
+#     driver_name = entries[0]['driver_name']
+#     line = entries[0]['line']
+#     date = entries[0]['date']
+#     date_split = date.split("-")
+#     date = date_split[-1] + "-" + date_split[-2] + "-" + date_split[-3]
+
+#     # Create table rows for products and their base amounts
+#     table_rows = []
+#     for item in entries:
+#         product_name = f"{item['product_name']}"
+
+#         # Add sales info with base amount
+#         table_rows.append(["ADD", product_name + " " + f"{item['cases']}C / {item['pieces']}P", item["base_amount"]])
+#         total_amount += item["base_amount"]
+
+
+#         # Add goods upload/return info
+#         table_rows.append(["INFO", f"Goods Upload: {item['goods_upload_cases']}C / {item['goods_upload_pieces']}P", 0])
+#         table_rows.append(["INFO", f"Goods Return: {item['goods_return_cases']}C / {item['goods_return_pieces']}P", 0])
+        
+#     # Add total before deductions
+#     table_rows.append(["", "Total Before Deductions", total_amount])
+#     table_rows.append(["", "---------------", "--------"])
+
+#     # Process all deductions
+#     deduction_rows = []
+#     for item in entries:
+#         # Add individual discounts
+#         for discount_entry in item.get('discount', []):
+#             discount_amount = (discount_entry['cases'] * discount_entry['size']) + (discount_entry['pieces'] * (discount_entry['size']/24))
+#             deduction_rows.append(["SUB", f"Discount ({discount_entry['size']}) - {item['product_name']}", discount_amount])
+#             discount += discount_amount
+
+#         # Add individual commissions
+#         for commission_entry in item.get('commission', []):
+#             commission_amount = (commission_entry['cases'] * commission_entry['size']) + (commission_entry['pieces'] * (commission_entry['size']/24))
+#             deduction_rows.append(["SUB", f"Commission ({commission_entry['size']}) - {item['product_name']}", commission_amount])
+#             commission += commission_amount
+
+#         if item.get("kuraivu_amount") is not None and item.get("kuraivu_amount") > 0:
+#             deduction_rows.append(["ADD", f"Kuraivu - {item['product_name']}", item["kuraivu_amount"]])
+        
+#         final_amount += item["final_amount"]
+
+#     # Add total deductions summary
+#     summary_rows = [
+#         ["SUB", "Total Discount", discount],
+#         ["SUB", "Total Commission", commission],
+#     ]
+
+#     # Add credits
+#     if credits["payable"] > 0:
+#         summary_rows.append(["SUB", "Credit Payable", credits["payable"]])
+#     if credits["receivable"] > 0:
+#         summary_rows.append(["ADD", "Credit Receivable", credits["receivable"]])
+
+#     # Add total after deductions
+#     summary_rows.append(["", "Total After Deductions", final_amount])
+#     # summary_rows.append(["", "---------------", "--------"])
+
+#     # Create expense rows
+#     expense_rows = []
+#     total_expenses = 0
+#     for description, amount in expenses.items():
+#         expense_rows.append(["SUB", description, amount])
+#         total_expenses += amount
+#     expense_rows.append(["", "Total Expenses", total_expenses])
+
+#     # Create final total row
+#     final_total = round(final_amount - total_expenses, 4) + (credits.get("receivable") - credits.get("payable") )
+#     final_total_row = ["", "FINAL TOTAL", final_total]
+
+#     # Generate table
+#     header = f"**Driver Name:** {driver_name}\n**Line:** {line}\n**Date:** {date}\n\n"
+#     table = tabulate(
+#         table_rows + deduction_rows + summary_rows + expense_rows + [final_total_row],
+#         headers=["", "Description", "Amount"],
+#         tablefmt="rounded_outline",
+#         maxcolwidths=[None, 10, None],
+#     )
+#     telegram_message = f"<pre>{header}{table}</pre>"
+
+#     return telegram_message
+
 def format_msg(data):
-    # Initialize variables
     total_amount = 0
     discount = 0
     commission = 0
@@ -50,20 +143,11 @@ def format_msg(data):
     # Create table rows for products and their base amounts
     table_rows = []
     for item in entries:
-        product_name = f"{item['product_name']}"
-
-        # Add sales info with base amount
-        table_rows.append(["ADD", product_name + " " + f"{item['cases']}C / {item['pieces']}P", item["base_amount"]])
         total_amount += item["base_amount"]
 
-
-        # Add goods upload/return info
-        table_rows.append(["INFO", f"Goods Upload: {item['goods_upload_cases']}C / {item['goods_upload_pieces']}P", 0])
-        table_rows.append(["INFO", f"Goods Return: {item['goods_return_cases']}C / {item['goods_return_pieces']}P", 0])
-        
     # Add total before deductions
     table_rows.append(["", "Total Before Deductions", total_amount])
-    table_rows.append(["", "---------------", "--------"])
+    # table_rows.append(["", "---------------", "--------"])
 
     # Process all deductions
     deduction_rows = []
@@ -71,13 +155,13 @@ def format_msg(data):
         # Add individual discounts
         for discount_entry in item.get('discount', []):
             discount_amount = (discount_entry['cases'] * discount_entry['size']) + (discount_entry['pieces'] * (discount_entry['size']/24))
-            deduction_rows.append(["SUB", f"Discount ({discount_entry['size']}) - {item['product_name']}", discount_amount])
+            # deduction_rows.append(["SUB", f"Discount ({discount_entry['size']}) - {item['product_name']}", discount_amount])
             discount += discount_amount
 
         # Add individual commissions
         for commission_entry in item.get('commission', []):
             commission_amount = (commission_entry['cases'] * commission_entry['size']) + (commission_entry['pieces'] * (commission_entry['size']/24))
-            deduction_rows.append(["SUB", f"Commission ({commission_entry['size']}) - {item['product_name']}", commission_amount])
+            # deduction_rows.append(["SUB", f"Commission ({commission_entry['size']}) - {item['product_name']}", commission_amount])
             commission += commission_amount
 
         if item.get("kuraivu_amount") is not None and item.get("kuraivu_amount") > 0:
@@ -99,13 +183,13 @@ def format_msg(data):
 
     # Add total after deductions
     summary_rows.append(["", "Total After Deductions", final_amount])
-    summary_rows.append(["", "---------------", "--------"])
+    # summary_rows.append(["", "---------------", "--------"])
 
     # Create expense rows
     expense_rows = []
     total_expenses = 0
     for description, amount in expenses.items():
-        expense_rows.append(["SUB", description, amount])
+        # expense_rows.append(["SUB", description, amount])
         total_expenses += amount
     expense_rows.append(["", "Total Expenses", total_expenses])
 
